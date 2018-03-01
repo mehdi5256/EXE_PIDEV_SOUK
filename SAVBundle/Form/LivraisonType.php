f6dcffa6cd1a54bb67a5bfb67f967c15c94d6234
Lhanafi <lobna.hanafi@esprit.tn>
2018-03-01 20:48:27 +0100

    Forms updates
    Forms updates

--- SAVBundle/Form/LivraisonType.php
+++ SAVBundle/Form/LivraisonType.php
@@ -2,25 +2,36 @@
 
 namespace SAVBundle\Form;
 
+use SAVBundle\Entity\FullAdress;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class LivraisonType extends AbstractType
 {
+
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('dateLivraison')->add('etat')->add('commandeRef')->add('livreurRef')->add('adresse');
-    }/**
+        $builder->add('dateLivraison')->add('adresses',EntityType::class, array(
+            'class' =>'SAVBundle\Entity\FullAdress',
+            'choices' =>$options['adresses'],
+            'choice_label' => function (FullAdress $rue){
+                return $rue->getRue();
+            }));
+    }
+    /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'SAVBundle\Entity\Livraison'
+            'data_class' => 'SAVBundle\Entity\Livraison',
+            'adresses' =>array()
         ));
     }
 
--- SAVBundle/Form/demandeLivreurType.php
+++ SAVBundle/Form/demandeLivreurType.php
@@ -3,6 +3,7 @@
 namespace SAVBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +14,32 @@ class demandeLivreurType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('dateInscription')->add('vehicule')->add('disponibilite');
-    }/**
+        $builder->add('Vehicule',ChoiceType::class, array(
+            'choices' => array(
+
+                "Voiture"=>"Voiture",
+                "Moto"=>"Moto",
+                "Camion"=>"Camion",
+
+            ),
+            'placeholder'=> "Veuillez choisir un véhicule",
+            'required'=>true
+        ))
+            ->add('disponibilite',ChoiceType::class, array(
+            'choices'=> array(
+                'Lundi' =>1,
+                'Mardi' =>2,
+                'Mercredi' =>3,
+                'Jeudi' =>4,
+                'Vendredi' =>5,
+                'Samedi' =>6,
+                'Dimanche' =>7),
+                'choices_as_values' => true,'multiple'=>true,'expanded'=>true
+                                                            )
+                 );
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
